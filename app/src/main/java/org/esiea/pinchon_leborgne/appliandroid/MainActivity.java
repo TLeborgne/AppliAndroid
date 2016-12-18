2dc9624a905fdce2407e81c5a83860d2e8412db3
TLebor <leborgne@et.esiea.fr>
2016-12-18 16:40:49 +0100

    Slide 27

--- app/src/main/java/org/esiea/pinchon_leborgne/appliandroid/MainActivity.java
+++ app/src/main/java/org/esiea/pinchon_leborgne/appliandroid/MainActivity.java
@@ -1,8 +1,11 @@
 package org.esiea.pinchon_leborgne.appliandroid;
 
 import android.app.DatePickerDialog;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.view.Menu;
@@ -37,10 +40,32 @@ public class MainActivity extends AppCompatActivity {
         };
         dpd = new DatePickerDialog(this, odsl, year, month, day);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.toastMenu:
+                toastMenuClicked();
+        }
+        return super.onOptionsItemSelected(item);
+    }
     public void button1Clicked(View v){
         Toast.makeText(getApplicationContext(),getString(R.string.msg),Toast.LENGTH_LONG).show();
+        NotificationCompat.Builder maNotification= new NotificationCompat.Builder(this);
+        maNotification.setContentTitle("Hello World!");
+        maNotification.setContentText("Bonjour le monde!");
+        maNotification.setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManager notifManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notifManager.notify(1, maNotification.build());
     }
     public void textViewClicked(View v){
         dpd.show();
+    }
+    public void toastMenuClicked(){
+        Toast.makeText(getApplicationContext(),getString(R.string.msgMenu),Toast.LENGTH_LONG).show();
     }
 }
